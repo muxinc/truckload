39eba8f8c7d4156137f9e86da2d25c13d43572e7
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2024-05-01 15:48:44 -0400

    Review text

--- app/components/shared/review.tsx
+++ app/components/shared/review.tsx
@@ -4,9 +4,16 @@ export default function Review() {
   return (
     <div>
       <Heading>Review</Heading>
-      <p className="mt-2">It is your responsibility to verify any anticipated billing charges before continuing.</p>
-      <p className="mt-2">By using this tool, you agree that you are liable for any actions taken.</p>
-      <p className="mt-2">Click &ldquo;Move Videos&rdquo; in the moving list to continue.</p>
+      <p className="mt-2">Before the big move, there&apos;s one important last step.</p>
+      <p className="mt-2">
+        Since your video providers likely charge for moving and storing files, make sure you understand any costs
+        you&apos;ll incur by migrating your videos.
+      </p>
+      <p className="mt-2">
+        By using Truckload, you agree that you are responsible for the actions taken on each platform as part of the
+        migration, including any fees your providers charge.
+      </p>
+      <p className="mt-2">Click &ldquo;Move Videos&rdquo; in the moving list to start your migration.</p>
     </div>
   );
 }
